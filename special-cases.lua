bd88c15329ee82e6763f6490d2278bde12295c9f
theRustyKnife <therustyknife13@gmail.com>
2016-11-26 21:47:23 +0100

    Changes to reflect CC 0.3

---
+++ special-cases.lua
@@ -0,0 +1,32 @@
+local rafting_combinator_data = crafting_combinator_data or {}
+
+-- items to be used as recipes to prevent unnecessary virtual recipe creation
+-- ["recipe-name"] = "item-name",
+crafting_combinator_data.overrides = {
+	--Item Collectors
+	["item-collector"] = "item-collector-area",
+	
+	--Bio Industries
+	["bi-liquid-air"] = "liquid-air",
+	["bi-nitrogen"] = "nitrogen",
+	["Bio_Cannon"] = "Bio_Cannon_Area",
+	["bi-biomass-0"] = "bi-biomass",
+	["bi-crushed-stone"] = "stone-crushed",
+	["bi-seedling"] = "seedling",
+}
+
+-- if the icon can't be found by the mod use this to assign it mannualy
+-- ["recipe-name"] = "__path__/to/icon",
+crafting_combinator_data.icons = {
+	--Senpais Overhall
+	["alien-artifact-from-small"] = "__base__/graphics/icons/alien-artifact.png",
+	
+	--Bio Industries
+	["bi-plastic"] = "__base__/graphics/icons/plastic-bar.png",
+	
+	--Switch Liquid IO
+	["flame-thrower-ammo-r"] = "__base__/graphics/icons/flame-thrower-ammo.png",
+	["sulfur-r"] = "__base__/graphics/icons/sulfur.png",
+}
+
+return crafting_combinator_data
